64457d4a3d387977358326257d5d6e851f2adffc
Maxim Stovba <maxim.stovba@gmail.com>
2022-09-02 11:37:41 +0300

    Update document

--- mongodb.js
+++ mongodb.js
@@ -19,23 +19,59 @@ MongoClient.connect(
 
     const db = client.db(databaseName)
 
-    db.collection('users')
-      .find({ age: 38 })
-      .toArray((error, users) => {
-        console.log(users)
-      })
+    // db.collection('users')
+    //   .find({ age: 39 })
+    //   .toArray((error, users) => {
+    //     console.log(users)
+    //   })
 
-    db.collection('tasks').findOne(
-      { _id: new ObjectId('630af2a57caa59a3283cfe74') },
-      (error, task) => {
-        console.log(task)
-      }
-    )
+    // db.collection('tasks').findOne(
+    //   { _id: new ObjectId('630af2a57caa59a3283cfe74') },
+    //   (error, task) => {
+    //     console.log(task)
+    //   }
+    // )
+
+    // db.collection('tasks')
+    //   .find({ completed: false })
+    //   .toArray((error, tasks) => {
+    //     console.log(tasks)
+    //   })
+
+    // db.collection('users')
+    //   .updateOne(
+    //     {
+    //       _id: new ObjectId('6307cd5ea4b9d57aa016670c'),
+    //     },
+    //     {
+    //       $inc: {
+    //         age: 1,
+    //       },
+    //     }
+    //   )
+    //   .then((result) => {
+    //     console.log(result)
+    //   })
+    //   .catch((error) => {
+    //     console.log(error)
+    //   })
 
     db.collection('tasks')
-      .find({ completed: false })
-      .toArray((error, tasks) => {
-        console.log(tasks)
+      .updateMany(
+        {
+          completed: false,
+        },
+        {
+          $set: {
+            completed: true,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result.modifiedCount)
+      })
+      .catch((error) => {
+        console.log(error)
       })
   }
 )
