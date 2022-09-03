4f126d600e386cd40cfaa0943166f716afc5ad2a
Maxim Stovba <maxim.stovba@gmail.com>
2022-09-03 22:25:22 +0300

    validator

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const { DB_URL } = process.env
 
@@ -11,37 +12,71 @@ mongoose.connect(`mongodb://${DB_URL}:27017/${databaseName}`)
 const User = mongoose.model('User', {
   name: {
     type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid')
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password cannot contain "password"')
+      }
+    },
   },
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a postive number')
+      }
+    },
   },
 })
 
-// const me = new User({
-//   name: 'Andrew',
-//   age: '31',
-// })
+const me = new User({
+  name: 'Maxim',
+  age: '39',
+  email: 'max@mail.com',
+  password: 'phone098!',
+})
 
-// me.save()
-//   .then(() => {
-//     console.log(me)
-//   })
-//   .catch((error) => {
-//     console.log('Error!', error)
-//   })
+me.save()
+  .then(() => {
+    console.log(me)
+  })
+  .catch((error) => {
+    console.log('Error!', error)
+  })
 
 const Task = mongoose.model('Task', {
   description: {
     type: String,
+    required: true,
+    trim: true,
   },
   completed: {
     type: Boolean,
+    default: false,
   },
 })
 
 const task = new Task({
-  description: 'Learn the Mongoose library',
-  completed: false,
+  description: 'Eat lunch',
 })
 
 task
