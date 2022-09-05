347827807e0e719143540e446a6ee7c5f9c418ec
Maxim Stovba <maxim.stovba@gmail.com>
2022-09-05 16:37:40 +0300

    Resource Reading Endpoints

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,28 +5,3 @@ const { DB_URL } = process.env
 const databaseName = 'task-manager'
 
 mongoose.connect(`mongodb://${DB_URL}:27017/${databaseName}`)
-
-// const Task = mongoose.model('Task', {
-//   description: {
-//     type: String,
-//     required: true,
-//     trim: true,
-//   },
-//   completed: {
-//     type: Boolean,
-//     default: false,
-//   },
-// })
-
-// const task = new Task({
-//   description: 'Eat lunch',
-// })
-
-// task
-//   .save()
-//   .then(() => {
-//     console.log(task)
-//   })
-//   .catch((error) => {
-//     console.log(error)
-//   })
