edbb864abc90c83206f04d6bad34a81433b7b5b6
Maxim Stovba <maxim.stovba@gmail.com>
2022-10-15 15:06:09 +0300

    Sorting, Pagination, and Filtering

--- src/index.js
+++ src/index.js
@@ -6,18 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -28,14 +16,3 @@ app.listen(port, () => {
 
 const Task = require('./models/task')
 const User = require('./models/user')
-
-const main = async () => {
-  // const task = await Task.findById('5c2e505a3253e18a43e612e6')
-  // await task.populate('owner').execPopulate()
-  // console.log(task.owner)
-  // const user = await User.findById('5c2e4dcb5eac678a23725b5b')
-  // await user.populate('tasks').execPopulate()
-  // console.log(user.tasks)
-}
-
-main()
